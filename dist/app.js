6fad4fb69809c96bf3f57522f135c366d2b56f68
Doug L <phena109@gmail.com>
2019-01-04 21:06:30 +1100

    smaller the click area (a bit)

--- dist/app.js
+++ dist/app.js
@@ -1,23 +1,23 @@
-$('.app-action-1').on('mouseup', function() {
+$('.app-action-1 .action-icon').on('mouseup', function() {
     document.querySelector('.action-audio-1').play();
 });
 
-$('.app-action-2').on('mouseup', function() {
+$('.app-action-2 .action-icon').on('mouseup', function() {
     document.querySelector('.action-audio-2').play();
 });
 
-$('.app-action-3').on('mouseup', function() {
+$('.app-action-3 .action-icon').on('mouseup', function() {
     document.querySelector('.action-audio-3').play();
 });
 
-$('.app-action-4').on('mouseup', function() {
+$('.app-action-4 .action-icon').on('mouseup', function() {
     document.querySelector('.action-audio-4').play();
 });
 
-$('.app-action-5').on('mouseup', function() {
+$('.app-action-5 .action-icon').on('mouseup', function() {
     document.querySelector('.action-audio-5').play();
 });
 
-$('.app-action-6').on('mouseup', function() {
+$('.app-action-6 .action-icon').on('mouseup', function() {
     document.querySelector('.action-audio-6').play();
 });
