f9b9a6dcffe66cd0637a76b964454e8608323f04
Doug L <phena109@gmail.com>
2019-01-04 18:38:04 +1100

    much more

---
+++ dist/app.js
@@ -0,0 +1,23 @@
+$('.app-action-1').on('mouseup', function() {
+    document.querySelector('.action-audio-1').play();
+});
+
+$('.app-action-2').on('mouseup', function() {
+    document.querySelector('.action-audio-2').play();
+});
+
+$('.app-action-3').on('mouseup', function() {
+    document.querySelector('.action-audio-3').play();
+});
+
+$('.app-action-4').on('mouseup', function() {
+    document.querySelector('.action-audio-4').play();
+});
+
+$('.app-action-5').on('mouseup', function() {
+    document.querySelector('.action-audio-5').play();
+});
+
+$('.app-action-6').on('mouseup', function() {
+    document.querySelector('.action-audio-6').play();
+});
